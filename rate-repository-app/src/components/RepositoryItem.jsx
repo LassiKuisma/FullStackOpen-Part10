a07a30e8c42b5043649866abae043335fa8952a4
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-06-15 19:45:12 +0300

    wip: repository stats

--- rate-repository-app/src/components/RepositoryItem.jsx
+++ rate-repository-app/src/components/RepositoryItem.jsx
@@ -1,17 +1,46 @@
-import { Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  logo: {
+    width: 50,
+    height: 50,
+  },
+});
 
 const RepositoryItem = (props) => {
   const item = props.item;
 
   return (
     <View>
+      <Image
+        style={styles.logo}
+        source={item.ownerAvatarUrl}
+        alt="user avatar"
+      />
       <Text>Full name: {item.fullName}</Text>
       <Text>Description: {item.description}</Text>
       <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+      <RepositoryStats item={item} />
+    </View>
+  );
+};
+
+const RepositoryStats = ({ item }) => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <StatsCell title={'Stars'} value={item.stargazersCount} />
+      <StatsCell title={'Forks'} value={item.forksCount} />
+      <StatsCell title={'Reviews'} value={item.reviewCount} />
+      <StatsCell title={'Rating'} value={item.ratingAverage} />
+    </View>
+  );
+};
+
+const StatsCell = ({ title, value }) => {
+  return (
+    <View>
+      <Text>{value}</Text>
+      <Text>{title}</Text>
     </View>
   );
 };
