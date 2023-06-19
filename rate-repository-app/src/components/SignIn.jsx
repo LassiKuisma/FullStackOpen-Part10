3a6e3d7a807e7ff799ac21897eeee9ff434b0c30
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-06-19 17:03:34 +0300

    wip: 10.8 login form

--- rate-repository-app/src/components/SignIn.jsx
+++ rate-repository-app/src/components/SignIn.jsx
@@ -1,7 +1,36 @@
+import { Pressable, View } from 'react-native';
+import { Formik } from 'formik';
+
+import FormikTextInput from './FormikTextInput';
 import Text from './Text';
 
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+  return (
+    <View>
+      <FormikTextInput name="username" placeholder="Username" />
+      <FormikTextInput name="password" placeholder="Password" />
+      <Pressable onPress={onSubmit}>
+        <Text>Login</Text>
+      </Pressable>
+    </View>
+  );
+};
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>
+  );
 };
 
 export default SignIn;
