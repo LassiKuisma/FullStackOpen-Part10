72a9e3ca60f17fa5b32a8ad6c2513b1617660273
Lassi Kuisma <lassi.kuisma1@gmail.com>
2023-07-21 15:56:19 +0300

    exercise 10.24

--- rate-repository-app/src/components/RepositoryList.jsx
+++ rate-repository-app/src/components/RepositoryList.jsx
@@ -1,9 +1,10 @@
-import { FlatList, View, StyleSheet, Pressable } from 'react-native';
+import { FlatList, View, StyleSheet, Pressable, TextInput } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native';
 
+import { useDebounce } from 'use-debounce';
 import { useState } from 'react';
 import theme from '../theme';
 
@@ -13,8 +14,16 @@ const styles = StyleSheet.create({
   },
   picker: {
     padding: 10,
+    margin: 10,
     fontSize: theme.fontSizes.body,
     fontWeight: theme.fontWeights.bold,
+  },
+  keywordFilter: {
+    placeholderTextColor: theme.colors.textSecondary,
+    margin: 10,
+    padding: 10,
+    borderRadius: 5,
+    borderWidth: 1,
   },
 });
 
@@ -49,11 +58,38 @@ const OrderingPicker = ({ ordering, setOrdering }) => {
   );
 };
 
+const KeywordFilter = ({ keyword, setKeyword }) => {
+  return (
+    <TextInput
+      style={styles.keywordFilter}
+      placeholder="Filter by keyword"
+      value={keyword}
+      onChangeText={(value) => setKeyword(value)}
+    />
+  );
+};
+
+const RepositoryListHeader = ({
+  ordering,
+  setOrdering,
+  keyword,
+  setKeyword,
+}) => {
+  return (
+    <View>
+      <KeywordFilter keyword={keyword} setKeyword={setKeyword} />
+      <OrderingPicker ordering={ordering} setOrdering={setOrdering} />
+    </View>
+  );
+};
+
 export const RepositoryListContainer = ({
   repositories,
   onPress,
   ordering,
   setOrdering,
+  keyword,
+  setKeyword,
 }) => {
   const repositoryNodes = repositories
     ? repositories.edges.map((edge) => edge.node)
@@ -64,7 +100,12 @@ export const RepositoryListContainer = ({
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
       ListHeaderComponent={
-        <OrderingPicker ordering={ordering} setOrdering={setOrdering} />
+        <RepositoryListHeader
+          ordering={ordering}
+          setOrdering={setOrdering}
+          keyword={keyword}
+          setKeyword={setKeyword}
+        />
       }
       renderItem={({ item }) => (
         <Pressable onPress={() => onPress(item.id)}>
@@ -77,8 +118,14 @@ export const RepositoryListContainer = ({
 
 const RepositoryList = () => {
   const [ordering, setOrdering] = useState('latestFirst');
+  const [keyword, setKeyword] = useState('');
+  const [debounced] = useDebounce(keyword, 500);
 
-  const { repositories } = useRepositories(orderingTypes[ordering]);
+  const args = {
+    keyword: debounced,
+    ...orderingTypes[ordering],
+  };
+  const { repositories } = useRepositories(args);
   const navigate = useNavigate();
 
   const onPress = (id) => {
@@ -91,6 +138,8 @@ const RepositoryList = () => {
       onPress={onPress}
       ordering={ordering}
       setOrdering={setOrdering}
+      keyword={keyword}
+      setKeyword={setKeyword}
     />
   );
 };
--- rate-repository-app/src/hooks/useRepositories.js
+++ rate-repository-app/src/hooks/useRepositories.js
@@ -5,12 +5,14 @@ import { GET_REPOSITORIES } from '../graphql/queries';
 const useRepositories = (args) => {
   const orderBy = args?.orderBy || 'CREATED_AT';
   const orderDirection = args?.orderDirection || 'DESC';
+  const keyword = args?.keyword || '';
 
   const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
     fetchPolicy: 'cache-and-network',
     variables: {
       orderBy,
       orderDirection,
+      searchKeyword: keyword,
     },
   });
 
